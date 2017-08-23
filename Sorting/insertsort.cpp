253245d4aef6c4a8f046598f15ad95d17c99cc1e
NMD-11/9/2015 <dvsolimeo@Gmail.com>
2017-08-23 23:01:23 +0200

    Update and rename insertsort.cpp to Sorting/insertsort.cpp

--- Sorting/insertsort.cpp
+++ Sorting/insertsort.cpp
@@ -13,14 +13,3 @@ void insertSort(std::list<int> &list)
     		}
   	}
 }
-/*
-int main(int argc, char const *argv[]) 
-{
-  std::list<int> array = {4, 5, 2, 3, 0, 1};
-  insertSort(array);
-  for(auto a: array) {
-    std::cout << a << " ";
-  }
-  std::cout << std::endl;
-  return 0;
-}*/
