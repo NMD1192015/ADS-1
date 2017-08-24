318f32183b6ceac8854d6f24568bde93e3628ac9
Giulio M <sperectrum@gmail.com>
2017-08-24 20:54:27 +0200

    Restyling quicksort

--- Sorting/quicksort.cpp
+++ Sorting/quicksort.cpp
@@ -1,34 +1,29 @@
-#include <iostream>
 #include <list>
 
 using namespace std;
 
-void quickSort(std::list<int> &list) 
-{
-  // pick the pivot element
-  const auto pivot = list.begin();
-  // iterate through the rest of the list
-  for (auto i = next(pivot); i != list.end(); ++i) 
-  {
-    // if the element is < of pivot, shift it before pivot
-  	if(*i < *pivot) list.splice(pivot, list, i, next(i));
-  }
-  
-  // we have now 3 lists, the left one,
-  // the one with just pivot inside and the right one
-  
-  std::list<int> rightList;
-  std::list<int> pivotList;
-  
-  rightList.splice(rightList.begin(), list, next(pivot), list.end());
-  
-  pivotList.splice(pivotList.begin(), list, pivot, next(pivot));
+void quickSort(std::list<int> &list) {
+    // pick the pivot element
+    const auto pivot = list.begin();
+    // iterate through the rest of the list
+    for (auto i = next(pivot); i != list.end(); ++i) {
+        // if the element is < of pivot, shift it before pivot
+        if(*i < *pivot) list.splice(pivot, list, i, next(i));
+    }
 
-  // if left or right are > 1, apply quicksort
-  if (list.size() > 1) quickSort(list);
-  if (rightList.size() > 1) quickSort(rightList);
+    // we have now 3 lists, the left one,
+    // the one with just pivot inside and the right one
+    std::list<int> rightList;
+    std::list<int> pivotList;
 
-  // merge the 2 lists back
-  list.splice(list.end(), pivotList);
-  list.splice(list.end(), rightList);
+    rightList.splice(rightList.begin(), list, next(pivot), list.end());
+    pivotList.splice(pivotList.begin(), list, pivot, next(pivot));
+
+    // if left or right are > 1, apply quicksort
+    if (list.size() > 1) quickSort(list);
+    if (rightList.size() > 1) quickSort(rightList);
+
+    // merge the 2 lists back
+    list.splice(list.end(), pivotList);
+    list.splice(list.end(), rightList);
 }
