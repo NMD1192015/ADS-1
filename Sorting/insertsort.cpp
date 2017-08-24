76ba9f84d8029735021162c22ff70a76e20df41f
Giulio M <sperectrum@gmail.com>
2017-08-24 21:02:40 +0200

    Commented insertsort

--- Sorting/insertsort.cpp
+++ Sorting/insertsort.cpp
@@ -1,15 +1,9 @@
-#include <iostream>
 #include <list>
 
-void insertSort(std::list<int> &list) 
-{
-	for(auto i = next(list.begin()); i != list.end(); ++i) 
-	{
-		for(auto j = i; j != list.begin() and *prev(j) > *j; --j) 
-		{
-     	 		
-     	 		std::swap(*prev(j), *j);
-    	
-    		}
-  	}
+void insertSort(std::list<int> &list)  {
+	for(auto i = next(list.begin()); i != list.end(); ++i) {
+		for(auto j = i; j != list.begin() and *prev(j) > *j; --j) {
+			std::swap(*prev(j), *j);
+		}
+	}
 }
