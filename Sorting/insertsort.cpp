09d6a82a6ce6f6b870603e48f456739533a1e39b
Giulio M <sperectrum@gmail.com>
2017-08-24 21:04:56 +0200

    Commented insertsort

--- Sorting/insertsort.cpp
+++ Sorting/insertsort.cpp
@@ -1,8 +1,14 @@
 #include <list>
 
+// NOTE: using swap for this algorithm is unefficent
+// but more clean for mental clarity
+
 void insertSort(std::list<int> &list)  {
+	// Iterate from the second element onward
 	for(auto i = next(list.begin()); i != list.end(); ++i) {
+		// From i, iterate backward
 		for(auto j = i; j != list.begin() and *prev(j) > *j; --j) {
+			// Pull down the lowest entry
 			std::swap(*prev(j), *j);
 		}
 	}
